a41782d6cf657bd50e8e6e1e540946957020aeca
nhuvancmu@gmail.com <nhuvancmu@gmail.com>
2025-03-22 00:47:23 +0700

    Logout

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ var express = require('express');
 require('dotenv').config();
 const http = require("http");
 const path = require("path");
+const Routes = require("./routes/index.js");
 const connectDB = require("./config/database.js");
+const cookieParser = require('cookie-parser');
+
 
 var app = express();
 const server = http.createServer(app);
 app.use(express.json());
-const Routes = require("./routes/index.js");
+app.use(cookieParser());
+
 
 Routes(app);
 
-connectDB();
 
+
+
+connectDB();
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // Route for user registration
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
+router.post('/logout',userController.logout);
 
 
 module.exports = router;
