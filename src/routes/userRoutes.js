a85adf502b147a83710e2c94cc780cb43889591e
nhuvancmu@gmail.com <nhuvancmu@gmail.com>
2025-03-23 01:12:08 +0700

    changePassword

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,6 +7,5 @@ const router = express.Router();
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.post('/logout',userController.logout);
-
-
+router.post('/changePassword',userController.changePassword);
 module.exports = router;
