c78a5363d4bd67045f5552a2584b1090ac0f89b7
nhuvancmu@gmail.com <nhuvancmu@gmail.com>
2025-03-29 23:53:26 +0700

    add product

---
+++ src/controllers/productController.js
@@ -0,0 +1,29 @@
+const Product = require("../models/productModel");
+
+
+exports.addProduct = async (req, res) => {
+    try {
+        const { title, description, images, starting_price, status } = req.body;
+
+        // Kiểm tra dữ liệu đầu vào
+        if (!title || !description || !images || !starting_price) {
+            return res.status(400).json({ message: "Vui lòng cung cấp đầy đủ thông tin sản phẩm!" });
+        }
+
+        // Tạo sản phẩm mới
+        const product = new Product({
+            title,
+            description,
+            images,
+            starting_price,
+            status
+        });
+
+        await product.save();
+
+        res.status(201).json({ message: "Thêm sản phẩm thành công!", product });
+    } catch (error) {
+        console.error("Lỗi khi thêm sản phẩm:", error);
+        res.status(500).json({ message: "Đã xảy ra lỗi khi thêm sản phẩm!" });
+    }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const userRoute = require('./userRoutes');
 const roleRoute = require('./roleRoutes');
+const categoryRoute = require('./categoryRoutes');
 
 module.exports = (app) => {
     app.use("/accounts", userRoute);
+    app.use("/category",categoryRoute);
     app.use("/role",roleRoute);
+    app.use("/product",roleRoute);
 };
